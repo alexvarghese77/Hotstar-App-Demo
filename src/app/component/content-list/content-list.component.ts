5c80e2a5a38d1d170551c36e17f25b046fb8a833
Alex Varghese <alex.varghese77@gmail.com>
2018-05-09 06:55:16 -0800

    content list added

--- src/app/component/content-list/content-list.component.ts
+++ src/app/component/content-list/content-list.component.ts
@@ -6,8 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./content-list.component.scss']
 })
 export class ContentListComponent implements OnInit {
-
-  constructor() { }
+   arr=[];
+  constructor() { 
+    this.arr = Array(100).fill(0).map((_, i) => i + 1);
+  }
 
   ngOnInit() {
   }
