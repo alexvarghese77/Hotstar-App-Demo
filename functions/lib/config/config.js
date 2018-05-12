0613767e565eaf8537bf1e00e8c24603440b9a14
Alex Varghese <alex.varghese77@gmail.com>
2018-05-12 19:59:54 +0530

    icons added

---
+++ functions/lib/config/config.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.config = {
+    apiKey: "AIzaSyCbVp7LFfBiPGehT5KJCp1gHPyuyV7qK4Y",
+    authDomain: "moviestar-app.firebaseapp.com",
+    databaseURL: "https://moviestar-app.firebaseio.com",
+    projectId: "moviestar-app",
+    storageBucket: "moviestar-app.appspot.com",
+    messagingSenderId: "604830704926"
+};
+//firebase.initializeApp(config);
+//# sourceMappingURL=config.js.map
